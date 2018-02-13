5d0152edf065a72062eff7a689bc1c725b5e48b2
blaedj <blaedj@gmail.com>
2018-02-13 13:07:02 -0600

    Make ScriptumService properly implement ScriptumService interface

--- internal/scriptum.go
+++ internal/scriptum.go
@@ -21,24 +21,30 @@ func NewScriptumService(storage store.FileStore, logger log.Logger) (*ScriptumSe
 	return &ScriptumService{store: storage, logger: logger}, nil
 }
 
-func (svc *ScriptumService) NewDocument(ctx context.Context, doc pb.Document) (pb.NewDocumentResponse, error) {
+func (svc ScriptumService) NewDocument(ctx context.Context, doc *pb.Document, ownerID string) (*pb.NewDocumentResponse, error) {
 	t := time.Unix(100000, 0)
 	entropy := rand.New(rand.NewSource(t.UnixNano()))
 	docID := ulid.MustNew(ulid.Timestamp(t), entropy)
 
-	err := svc.store.Save(doc.Contents, docID.String())
+	err := svc.store.Save(doc.Contents, docID.String(), ownerID)
 	if err != nil {
-		return pb.NewDocumentResponse{
+		return &pb.NewDocumentResponse{
 			Err:        fmt.Sprintf("%v", err),
 			DocumentId: "",
 		}, err
 	}
 
-	return pb.NewDocumentResponse{
+	return &pb.NewDocumentResponse{
 		Err:        "",
 		DocumentId: docID.String(),
 	}, nil
 }
 
-// func (svc *ScriptumService) DeleteDocument(ctx context.Context, req pb.DeleteDocumentRequest) (pb.DeleteDocumentResponse, error) {
-// }
+func (svc ScriptumService) DeleteDocument(ctx context.Context, req *pb.DeleteDocumentRequest) (*pb.DeleteDocumentResponse, error) {
+	return &pb.DeleteDocumentResponse{Err: ""}, nil
+}
+
+//SaveDocument(context.Context, *Document) (*SaveDocumentResponse, error)
+func (svc ScriptumService) SaveDocument(ctx context.Context, doc *pb.Document) (*pb.SaveDocumentResponse, error) {
+	// noop for now
+}
